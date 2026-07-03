b3961e6318515311a7ef86bc94c42a4de021490e
Devjabbar <momohabduljabbar@gmail.com>
2026-07-03 12:04:04 +0100

    new updates

--- NodejsPractice/src/index.ts
+++ NodejsPractice/src/index.ts
@@ -1,56 +1,17 @@
-// index.js
 import express from "express";
-import { connectDB } from "./config/db";
 import dotenv from "dotenv";
-import classRoutes from "./routes/classRoutes";
-import testRoutes from "./routes/testRoutes";
-
-// import "./utils/jwtPractice";
-
-import "./utils/jwt";
-
-const app = express();
-
-{
-  /** 
-   
-  app.get("/", (req, res) => {
-    res.send("Hello, Node.js i am jabbar");
-  });
-
-  app.post("/data", (req, res) => {
-    console.log("Request body:", req.body); // logs JSON data sent from browser
-    res.send("Data received");
-  });
-
-  app.use((req, res, next) => {
-    console.log(`${req.method} `); // logs GET /, POST /api, etc.
-    next();
-  });
-
-  app.post("/user", (req, res) => {
-    console.log(req.body);
-  });
-
-  **/
-}
+import cors from "cors";
 
 dotenv.config();
+const app = express();
+console.log("i am cors", cors());
+app.use(cors());
 
-app.use(express.json());
-
-connectDB();
-
-// 👉 NEW: class routes (controller → service → repository → model)
-app.use("/api/classes", classRoutes);
-
-app.use("/api/test", testRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+app.post("/", (req, res, next) => {
+  console.log("i am the request", req.method);
+  res.send("i am a middleware 1");
 });
 
-const PORT = 2000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
